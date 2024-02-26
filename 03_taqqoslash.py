086687f08767bc2d152fc8e8a1d55ecca403deeb
Yellow4554Mouse <yellowmause@gmail.com>
2024-02-26 16:06:57 +0500

    tugatildi 26.02.2024

---
+++ 03_taqqoslash.py
@@ -0,0 +1,12 @@
+def taqqoslash(a,b):
+    if a==b :
+        print("Javob: A teng B ga! ")
+    elif a>b:
+        print("Javob: A katta B dan! ")
+    elif a<b :
+        print("Javob: A kichik B dan! ")
+    else:
+        print("ahmoq bo'lma, dasturni qiynama!!!! ")
+a = int(input("A ga qiymat bering: "))
+b = int(input("B ga qiymat bering: "))
+taqqoslash(a,b)
---
+++ 04_dictionary.py
@@ -0,0 +1,15 @@
+#Foydalanuvchidan aylaning radiusini qabul qilib olib, uning radiusini, diametrini, perimetri va yuzini 
+#lug'at ko'rinishida qaytaruvchi funksiya yozing >>python dictionary
+radius = int(input("iltimos aylananig radiusini kiriting: "))
+
+diameter= radius*2
+perimeter= radius*6.2832
+area = radius * 3.1416
+
+thisdict = {
+    radius : "radius",
+    diameter : "diametr",
+    perimeter : "perametr",
+    area : "yuzi"
+}
+print( thisdict  )
